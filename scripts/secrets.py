a9af75a6371bc1607f1cc58fc5a59cd65162849a
undefx <dfarrow0@gmail.com>
2016-10-28 23:11:24 -0700

    added python secrets

---
+++ scripts/secrets.py
@@ -0,0 +1,11 @@
+class db:
+
+  class username:
+    auto = '{SECRET_DB_USERNAME_AUTO}'
+    backup = '{SECRET_DB_USERNAME_BACKUP}'
+    epi = '{SECRET_DB_USERNAME_EPI}'
+
+  class password:
+    auto = '{SECRET_DB_PASSWORD_AUTO}'
+    backup = '{SECRET_DB_PASSWORD_BACKUP}'
+    epi = '{SECRET_DB_PASSWORD_EPI}'
