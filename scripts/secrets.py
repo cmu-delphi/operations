e6daa34512c3d84a597af8c40325e4bb26d6e801
undefx <dfarrow0@gmail.com>
2016-10-28 23:27:33 -0700

    more organized

--- scripts/secrets.py
+++ scripts/secrets.py
@@ -1,11 +1,5 @@
 class db:
 
-  class username:
-    auto = '{SECRET_DB_USERNAME_AUTO}'
-    backup = '{SECRET_DB_USERNAME_BACKUP}'
-    epi = '{SECRET_DB_USERNAME_EPI}'
-
-  class password:
-    auto = '{SECRET_DB_PASSWORD_AUTO}'
-    backup = '{SECRET_DB_PASSWORD_BACKUP}'
-    epi = '{SECRET_DB_PASSWORD_EPI}'
+  auto = ('{SECRET_DB_USERNAME_AUTO}', '{SECRET_DB_PASSWORD_AUTO}')
+  backup = ('{SECRET_DB_USERNAME_BACKUP}', '{SECRET_DB_PASSWORD_BACKUP}')
+  epi = ('{SECRET_DB_USERNAME_EPI}', '{SECRET_DB_PASSWORD_EPI}')
