b4741ebb3d0126aace779fb1939c29262f882d90
undefx <dfarrow0@gmail.com>
2016-11-08 21:43:22 -0500

    reorganize, php secrets

---
+++ secrets/secrets.php
@@ -0,0 +1,11 @@
+<?php
+class Secrets {
+
+  public static $db = array(
+    'auto' => array('{SECRET_DB_USERNAME_AUTO}', '{SECRET_DB_PASSWORD_AUTO}'),
+    'epi' => array('{SECRET_DB_USERNAME_EPI}', '{SECRET_DB_PASSWORD_EPI}')
+  );
+
+}
+?>
+
