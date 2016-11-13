7c7bfb048bdf9df25bf2884fffe761b41f41059e
undefx <dfarrow0@gmail.com>
2016-11-13 01:03:34 -0500

    add api secrets

--- secrets/secrets.php
+++ secrets/secrets.php
@@ -6,5 +6,15 @@ class Secrets {
     'epi' => array('{SECRET_DB_USERNAME_EPI}', '{SECRET_DB_PASSWORD_EPI}')
   );
 
+  public static $api = array(
+    'twitter' => '{SECRET_API_AUTH_TWITTER}',
+    'ght' => '{SECRET_API_AUTH_GHT}',
+    'signals' => '{SECRET_API_AUTH_SIGNALS}',
+    'ilinet' => '{SECRET_API_AUTH_ILINET}',
+    'stateili' => '{SECRET_API_AUTH_STATEILI}',
+    'cdc' => '{SECRET_API_AUTH_CDC}',
+    'sensors' => '{SECRET_API_AUTH_SENSORS}'
+  );
+
 }
 ?>
--- secrets/secrets.py
+++ secrets/secrets.py
@@ -3,3 +3,14 @@ class db:
   auto = ('{SECRET_DB_USERNAME_AUTO}', '{SECRET_DB_PASSWORD_AUTO}')
   backup = ('{SECRET_DB_USERNAME_BACKUP}', '{SECRET_DB_PASSWORD_BACKUP}')
   epi = ('{SECRET_DB_USERNAME_EPI}', '{SECRET_DB_PASSWORD_EPI}')
+
+
+class api:
+
+  twitter = '{SECRET_API_AUTH_TWITTER}'
+  ght = '{SECRET_API_AUTH_GHT}'
+  signals = '{SECRET_API_AUTH_SIGNALS}'
+  ilinet = '{SECRET_API_AUTH_ILINET}'
+  stateili = '{SECRET_API_AUTH_STATEILI}'
+  cdc = '{SECRET_API_AUTH_CDC}'
+  sensors = '{SECRET_API_AUTH_SENSORS}'
