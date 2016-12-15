ae970b92e490081a97a9e97c4d9c817bf6dd828f
undefx <dfarrow0@gmail.com>
2016-12-15 12:45:10 -0500

    backup and heart monitor

---
+++ src/backup.py
@@ -0,0 +1,87 @@
+import subprocess
+import datetime
+import os
+import time
+import secrets
+
+#General setup
+dest = '/home/automation/backups'
+tag = datetime.datetime.today().strftime('%Y%m%d_%H%M%S')
+print('Destination: %s | Tag: %s'%(dest, tag))
+
+#Directories
+dirs = [
+  {
+    'label': 'auto',
+    'path': '/home/automation',
+    'name': 'driver',
+    'exclude': 'flu_data',
+  },{
+    'label': 'html',
+    'path': '/var/www',
+    'name': 'html',
+  },{
+    'label': 'data',
+    'path': '/home/automation/driver',
+    'name': 'flu_data',
+  },
+]
+
+#Databases
+dbs = [
+  'automation',
+  'epicast2',
+  'epidata',
+  'utils',
+]
+
+#Utils
+def get_size(file):
+  return str(subprocess.check_output('du -h %s/%s'%(dest, file), shell=True), 'utf-8').split('\t')[0]
+
+#Each directory is backed up to it's own archive (*.tgz)
+#All databases are backed up at once to a single archive (*.sql.gz)
+#This list of archives is used to create a final archive containing all the others
+archives = []
+
+#Backup directories
+for dir in dirs:
+  print(' Directory: %s/%s'%(dir['path'], dir['name']))
+  if 'exclude' not in dir or dir['exclude'] is None:
+    exclude = ''
+  else:
+    exclude = '--exclude %s'%(dir['exclude'])
+  file = 'backup_%s_%s.tgz'%(tag, dir['label'])
+  subprocess.check_call('tar -czf %s/%s -C %s %s %s'%(dest, file, dir['path'], exclude, dir['name']), shell=True)
+  print(' %s'%(get_size(file)))
+  archives.append(file)
+
+#Backup databases
+list = ' '.join(dbs)
+print(' Databases: %s'%(list))
+file = 'backup_%s_database.sql'%(tag)
+u, p = secrets.db.backup
+subprocess.check_call('mysqldump --user=%s --password=%s --databases %s > %s/%s'%(u, p, list, dest, file), shell=True)
+subprocess.check_call('gzip %s/%s'%(dest, file), shell=True)
+file += '.gz'
+print(' %s'%(get_size(file)))
+archives.append(file)
+
+#Create the final archive
+print(' Building final archive')
+file = 'backup_%s.tar'%(tag)
+final_archive = '%s/%s'%(dest, file)
+subprocess.check_call('tar -cf %s -C %s %s'%(final_archive, dest, ' '.join(archives)), shell=True)
+print(' %s'%(get_size(file)))
+
+#Delete the intermediate archives
+for file in archives:
+  os.remove('%s/%s'%(dest, file))
+
+#Send the backup to an external drive
+subprocess.check_call('cp -v %s /mnt/usb500g/backups/'%(final_archive), shell=True)
+
+# TODO: Send the backup offsite
+
+#Success
+print('Backup completed successfully!')
---
+++ src/heart_monitor.py
@@ -0,0 +1,66 @@
+import argparse
+import time
+import mysql.connector
+from emailer import _send_email
+import secrets
+
+
+def check_heartbeat(name, timeout):
+  # connect to the database
+  try:
+    u, p = secrets.db.auto
+    cnx = mysql.connector.connect(user=u, password=p, database='automation')
+  except:
+    print('warning: unable to connect to the database!')
+    return
+  cur = cnx.cursor()
+
+  # check the heartbeat
+  cur.execute("SELECT unix_timestamp(now()) - unix_timestamp(date) FROM heartbeats WHERE name = %s", (name,))
+  delta = None
+  for (delta,) in cur:
+    pass
+
+  exit = False
+  # handle the heartbeat
+  if delta is None:
+    # couldn't read it
+    print('failed to check heartbeat')
+  else:
+    if delta < timeout:
+      # good to go
+      cur.execute("UPDATE heartbeats SET date = now() WHERE name = 'heart_monitor.py'")
+    else:
+      # blow it up
+      email_from = secrets.flucontest.email_epicast
+      email_to = secrets.flucontest.email_maintainer
+      email_subject = 'Heart Monitor'
+      email_body = 'Timeout exceeded for %s: %d >= %d' % (name, delta, timeout)
+      _send_email(email_from, email_to, email_subject, email_body)
+      exit = True
+
+  # cleanup
+  cur.close()
+  cnx.commit()
+  cnx.close()
+
+  # blow it up
+  if exit:
+    raise Exception('heartbeat exceeded timeout: %d >= %d'%(delta, timeout))
+
+if __name__ == '__main__':
+  # args and usage
+  parser = argparse.ArgumentParser()
+  parser.add_argument('name', type=str, help='script name (ex: automation.pl)')
+  parser.add_argument('timeout', type=int, help='timeout value in seconds (ex: 900)')
+  parser.add_argument('interval', type=int, help='update interval in seconds (ex: 60)')
+  args = parser.parse_args()
+
+  name = args.name #'automation.pl'
+  timeout = args.timeout #900
+  interval = args.interval #60
+
+  print('Checking %s within %d seconds, every %d seconds'%(name, timeout, interval))
+  while True:
+    check_heartbeat(name, timeout)
+    time.sleep(interval)
