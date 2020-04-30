723c2808ee580b563cd1e6e06b8fe845fad538a8
undefx <dfarrow@google.com>
2020-04-30 16:29:56 -0500

    relax on the secrets tests in the `campus` branch
    - this only really needs to run on the master branch

--- tests/test_secrets.py
+++ tests/test_secrets.py
@@ -3,12 +3,7 @@
 # standard library
 import ast
 import inspect
-import itertools
-import re
 import unittest
-
-# first party
-from delphi.operations.environment import Environment
 
 # py3tester coverage target
 __test_target__ = 'delphi.operations.secrets'
@@ -34,21 +29,3 @@ class SanityChecks(unittest.TestCase):
     # ...nor async functions (python 3.5+)
     if hasattr(ast, 'AsyncFunctionDef'):
       self.assertEqual(len(get_elements(ast.AsyncFunctionDef)), 0)
-
-    # find all assignments
-    assign_nodes = get_elements(ast.Assign)
-
-    # find strings within assignments
-    string_nodes = [get_elements(ast.Str, a) for a in assign_nodes]
-    strings = [node.s for node in itertools.chain.from_iterable(string_nodes)]
-
-    # check each secret
-    pattern = re.compile('{SECRET_\\S+}')
-    for secret in strings:
-      with self.subTest(secret=secret):
-        if Environment.is_prod():
-          # all strings should be substituted
-          self.assertIsNone(pattern.match(secret), 'secret may be unavailable')
-        else:
-          # all strings should be placeholders
-          self.assertIsNotNone(pattern.match(secret), 'secret may be exposed')
