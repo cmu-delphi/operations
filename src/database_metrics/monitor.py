e5a5f7cbf736cc3aee70686c4ea8c0834b55e1a6
andrew <chinandrew96@gmail.com>
2020-11-17 12:30:42 -0800

    add draft of measurement function

--- src/database_metrics/monitor.py
+++ src/database_metrics/monitor.py
@@ -1,9 +1,79 @@
 import multiprocessing as mp
 import time
+from functools import partial
 from typing import Callable
 
 from docker.models.containers import Container
-from .db_actions import _get_epidata_db_size
+from docker import DockerClient
+from .db_actions import _get_epidata_db_size, _clear_db
+from .actions import load_data, update_meta, send_query
+
+
+def measure_database(datasets: list,
+                     client: DockerClient,
+                     db_container: Container,
+                     queries: list = [],
+                     append_data: bool=False) -> dict:
+    """
+    Measure performance metrics for a list of functions and datasets.
+
+    For each dataset in `datasets`, measure load time, metadata update time, and optional queries.
+    Datasets are specified by a tuple of (source, file_pattern).
+    e.g. ("usa-facts", "202003*_county*"), which will load all the county level usa-facts data
+    for March 2020.
+
+    Examples
+    ________
+    # Measure metrics for load and metadata of usa-facts and fb-survey independently.
+    measure_database([("fb-survey", "*"), ("usa-facts", "*")],
+                     client,
+                     container)
+
+    # Measure metrics for load, metadata, and a query on March and March+April usa-facts data.
+    query1 = {"source": "covidcast",
+              "data_source": "usa-facts",
+              "signal": "deaths_incidence_num",
+              "time_type": "day",
+              "geo_type": "county",
+              "time_values": "20200301-2020501",
+              "geo_value": "*"}
+    measure_database([("usa-facts", "202003*_county*"), ("usa-facts", "202004*_county*")],
+                     client,
+                     container,
+                     [query1],
+                     append_data=True)
+
+    Parameters
+    ----------
+    datasets: list of tuples
+        List of 2-tuples defined as (source, patterns for files) to be included in each dataset.
+    client: DockerClient
+        DockerClient object to access and execute python images.
+    db_container: Container
+        Docker container object containing the database to measure.
+    queries: list of dictionaries, optional
+        List of query parameters to test query runtimes on.
+    append_data: boolean, optional
+        Boolean for whether to append each dataset onto the previous one (True), or clear the
+        database for each dataset (False). Defaults to False.
+
+    Returns
+    -------
+    Dictionary of metrics. Keys will be the datasets and values will be dicts containing the output
+    of parse_metrics() for loading, metadata updates, and queries.
+    """
+    output = {}
+    query_funcs = [partial(send_query, params=p) for p in queries]
+    meta_func = partial(update_meta, client=client)
+    for dataset in datasets:
+        load_func = partial(load_data, client=client, source=dataset[0], file_pattern=datasets[1])
+        if not append_data:
+            _clear_db(db_container)
+        output[dataset]["load"] = get_metrics(load_func, db_container)
+        output[dataset]["meta"] = get_metrics(meta_func, db_container)
+        for i, query in enumerate(query_funcs):
+            output[dataset][f"query{i}"] = get_metrics(query, db_container)
+    return output
 
 
 def get_metrics(func: Callable, container: Container) -> tuple:
