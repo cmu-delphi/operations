d52dbc59bafb0232628186548ad01b4bd9a32119
andrew <chinandrew96@gmail.com>
2020-11-18 10:08:41 -0800

    add second dataset

--- tests/database_metrics/test_monitor.py
+++ tests/database_metrics/test_monitor.py
@@ -25,20 +25,26 @@ class TestMonitor(unittest.TestCase):
             {"table_rows": 1, "db_disk_usage_mb": 2., "runtime": 3, "memory_usage_mb": 4},
             {"table_rows": 5, "db_disk_usage_mb": 6., "runtime": 7, "memory_usage_mb": 8},
             {"table_rows": 9, "db_disk_usage_mb": 10., "runtime": 11, "memory_usage_mb": 12},
+            {"table_rows": 2, "db_disk_usage_mb": 4., "runtime": 6, "memory_usage_mb": 8},
+            {"table_rows": 1, "db_disk_usage_mb": 3., "runtime": 5, "memory_usage_mb": 7},
+            {"table_rows": 3, "db_disk_usage_mb": 6., "runtime": 9, "memory_usage_mb": 12},
         ]
         expected = {
-            "datasets": [("a", "b")],
+            "datasets": [("a", "b"), ("c", "d")],
             "append_datasets": False,
             "load": [
-                {"table_rows": 1, "db_disk_usage_mb": 2., "runtime": 3, "memory_usage_mb": 4}
+                {"table_rows": 1, "db_disk_usage_mb": 2., "runtime": 3, "memory_usage_mb": 4},
+                {"table_rows": 2, "db_disk_usage_mb": 4., "runtime": 6, "memory_usage_mb": 8}
             ],
             "meta": [
-                {"table_rows": 5, "db_disk_usage_mb": 6., "runtime": 7, "memory_usage_mb": 8}
+                {"table_rows": 5, "db_disk_usage_mb": 6., "runtime": 7, "memory_usage_mb": 8},
+                {"table_rows": 1, "db_disk_usage_mb": 3., "runtime": 5, "memory_usage_mb": 7}
             ],
             "query0": [
-                {"table_rows": 9, "db_disk_usage_mb": 10., "runtime": 11, "memory_usage_mb": 12}
+                {"table_rows": 9, "db_disk_usage_mb": 10., "runtime": 11, "memory_usage_mb": 12},
+                {"table_rows": 3, "db_disk_usage_mb": 6., "runtime": 9, "memory_usage_mb": 12}
             ],
         }
-        output = monitor.measure_database([("a", "b")], None, None, ["q1"])
+        output = monitor.measure_database([("a", "b"), ("c", "d")], None, None, ["q1"])
         self.assertDictEqual(output, expected)
-        mock_clear.assert_called_once()
+        self.assertEqual(mock_clear.call_count, 2)
