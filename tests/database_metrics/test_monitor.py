24d86e9366dd5b3476cb50150dadc3e625ee9378
andrew <chinandrew96@gmail.com>
2020-11-20 10:04:40 -0800

    update test

--- tests/database_metrics/test_monitor.py
+++ tests/database_metrics/test_monitor.py
@@ -14,11 +14,8 @@ class TestMonitor(unittest.TestCase):
     @patch("delphi.operations.database_metrics.monitor.parse_metrics")
     @patch("delphi.operations.database_metrics.monitor.get_metrics")
     def test_measure_database(self, mock_metrics, mock_parser, mock_clear):
-        # mock_container = MagicMock()
-        # mock_container.exec_run.return_value = None
-        # mock_docker_client = MagicMock()
-        # mock_docker_client.containers.run.return_value = mock_container
-
+        mock_client = MagicMock()
+        mock_client.containers.get.return_value = None
         mock_metrics.return_value = None
         mock_clear.return_value = None
         mock_parser.side_effect = [
@@ -29,9 +26,12 @@ class TestMonitor(unittest.TestCase):
             {"table_rows": 1, "db_disk_usage_mb": 3., "runtime": 5, "memory_usage_mb": 7},
             {"table_rows": 3, "db_disk_usage_mb": 6., "runtime": 9, "memory_usage_mb": 12},
         ]
+        test_datasets = [("a", "b"), ("c", "d")]
+        test_queries = ["q0"]
         expected = {
-            "datasets": [("a", "b"), ("c", "d")],
+            "datasets": test_datasets,
             "append_datasets": False,
+            "queries": test_queries,
             "load": [
                 {"table_rows": 1, "db_disk_usage_mb": 2., "runtime": 3, "memory_usage_mb": 4},
                 {"table_rows": 2, "db_disk_usage_mb": 4., "runtime": 6, "memory_usage_mb": 8}
@@ -45,6 +45,6 @@ class TestMonitor(unittest.TestCase):
                 {"table_rows": 3, "db_disk_usage_mb": 6., "runtime": 9, "memory_usage_mb": 12}
             ],
         }
-        output = monitor.measure_database([("a", "b"), ("c", "d")], None, None, ["q1"])
+        output = monitor.measure_database(test_datasets, mock_client, "container", "image", test_queries)
         self.assertDictEqual(output, expected)
         self.assertEqual(mock_clear.call_count, 2)
